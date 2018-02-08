2cc90e27c1a988a8993173653516ba564c6c4eb0
Oscar <oscarbranson@gmail.com>
2018-02-08 15:42:27 +1100

    additional unit converters, add Rae (2018) full mass-balance B isotope calculation

--- inorg_b/helpers.py
+++ inorg_b/helpers.py
@@ -20,10 +20,32 @@ def sol_B_iso(BT, BO4, d11B_total, alpha=1.026):
     BO3 = BT - BO4
 
     d11BO4 = (d11B_total * BT - eps * BO3) / (BO4 + alpha * BO3)
-    d11BO3 = d11B_total - d11BO4
+    d11BO3 = d11BO4 + eps
 
     return d11BO4, d11BO3
 
+def sol_B_iso_Rae2018(pH, BO4, BT, alpha, d11BT):
+    """
+    Calculate d11B of aqueous species using full mass-balance approach of Rae, 2018.
+    
+    Returns
+    -------
+    d11BO4, d11BO3
+    """
+
+    R_BT = d11_2_R11(d11BT)
+    Hval = 10**-pH
+    Kbval = BO4 * Hval / (BT - BO4)
+    RB4 = (((Hval**2*R_BT**2 + 2*Hval**2*R_BT*alpha + Hval**2*alpha**2 + 2*Hval*Kbval*R_BT**2*alpha - 
+             2*Hval*Kbval*R_BT*alpha**2 + 8*Hval*Kbval*R_BT*alpha - 2*Hval*Kbval*R_BT + 2*Hval*Kbval*alpha + 
+             Kbval**2*R_BT**2*alpha**2 + 2*Kbval**2*R_BT*alpha + Kbval**2)**(1/2) - 
+            Hval*alpha - Kbval + Hval*R_BT + Kbval*R_BT*alpha)/(2*alpha*(Hval + Kbval)))
+    RB3 = RB4 * alpha
+
+    # return RB4, RB3
+    return R11_2_d11(RB4), R11_2_d11(RB3)
+
+# Unit Converters
 def d11_2_A11(d11, SRM_ratio=4.04367):
     """
     Convert Delta to Abundance notation.
@@ -39,6 +61,38 @@ def A11_2_d11(A11, SRM_ratio=4.04367):
     Default SRM_ratio is NIST951 11B/10B
     """
     return ((A11 / (1 - A11)) / SRM_ratio - 1) * 1000
+
+def A11_2_R11(A11):
+    """
+    Convert Abundance to isotope ratio notation.
+    """
+    return A11 / (1 - A11)
+
+def d11_2_R11(d11, SRM_ratio=4.04367):
+    """
+    Convert Delta to isotope ratio notation.
+
+    Default SRM_ratio is NIST951 11B/10B
+    """
+    return (d11/1000 + 1) * SRM_ratio
+
+
+def R11_2_d11(R11, SRM_ratio=4.04367):
+    """
+    Convert isotope ratio to Delta notation.
+
+    Default SRM_ratio is NIST951 11B/10B
+    """
+    return (R11 / SRM_ratio - 1) * 1000
+
+
+def R11_2_A11(R11):
+    """
+    Convert isotope ratio to abundance notation.
+    """
+    return R11 / (1 + R11)
+
+
 
 def extract_model_vars(rd, exp='Uchikawa', Rvar=('Solid', 'logR'), phase='Calcite'):
     """
